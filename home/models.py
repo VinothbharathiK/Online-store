37fe12210ff43b807e9e82bd55b133b293f82940
vinoth bharathi <kvinothbharathi1@gmail.com>
2025-03-15 23:44:00 +0530

    login and logout function added successfully and product sale and not sale updated

--- home/models.py
+++ home/models.py
@@ -27,6 +27,8 @@ class Product(models.Model):
     description=models.CharField(max_length=100)
     detail_description=models.TextField(blank=True)
     image=models.ImageField(upload_to='images/',blank=True)
+    is_sale=models.BooleanField(default=True)
+
     def __str__(self):
         return self.name
         
@@ -37,7 +39,7 @@ class Product(models.Model):
         return '0%'
     
 
-class Product(models.Model):
+class Slider(models.Model):
     name=models.CharField(max_length=20)
     price=models.DecimalField(max_digits=7,decimal_places=1)
     original_price=models.DecimalField(max_digits=7,decimal_places=1)
--- home/views.py
+++ home/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render,redirect
+from .models import Product,Slider
+from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def home(request):
     products=Product.objects.all()
-    return render(request,'index.html',{'products':products})
+    slides=Slider.objects.all()
+    context={
+        'products':products,
+        'slides':slides
+    }
+    return render(request,'index.html',context)
+
+def login_user(request):
+    if request.method == "POST": 
+        username =request.POST['username']
+        password =request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None :
+            login(request,user)
+            messages.success(request,("login succesfll"))
+            return redirect('home')
+        else:
+            messages.success(request,("invalid credentials"))
+            return redirect ('login')    
+    else:
+        return render (request,'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,("logout succesfull"))    
+    return redirect('home')
+
+
